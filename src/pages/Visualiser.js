bc0c1aa8bdfe052e76d59bc20a677373766b2a23
GiuliaP95 <kirakira95@msn.com>
2022-06-21 19:12:25 +0200

    Skeleton for canvas. To do: - customise palette for different themes; - check navbar routing works.

--- src/pages/Visualiser.js
+++ src/pages/Visualiser.js
@@ -1,7 +1,71 @@
 import React from "react";
+import {useEffect} from "react";
+import style from "../style/visualiser.module.scss"
+//note: 3 possible youtube links i.e. 3 possible ways that they key will be embedded
+//https://www.npmjs.com/package/youtube-mp3-downloader
+
+
+
+
+function mp3_to_hist(filename){
+    let song = new Audio(filename);
+    const audioContext = new AudioContext();
+    const audioSource = audioContext.createMediaElementSource(song);
+    const analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const tick = () => {
+        analyser.getByteTimeDomainData(dataArray);
+        //console.log(dataArray);
+        requestAnimationFrame(tick);
+    }
+
+    // song.play();
+    song.pause();
+    //tick();
+
+    return 'hi !'
+}
+
 
 export default function Visualiser(){
+
+    useEffect(() => {
+        console.log(mp3_to_hist('default.mp3'));
+
+        draw(canvasRef.current);
+    }, []);
+
+    const canvasRef = React.createRef();
+
+    const draw = (cRef) =>{
+        cRef.width=500;
+        cRef.height=500;
+
+        const ctx = cRef.getContext("2d");
+        const gradient = ctx.createLinearGradient(0, 0, 0, cRef.height);
+        gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+        gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
+        ctx.fillStyle = gradient;
+
+        const lineColor = "rgb(" + 100 + ", " + 100 + ", " + 205 + ")";
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = 10;
+        ctx.beginPath();
+        ctx.moveTo(5, 10);
+        ctx.lineTo(100, 200);
+        ctx.stroke();
+    }
+
+    //move everything in here
     return(
-        <h1>Visualiser</h1>
+        <section className={style.visualiser}>
+            <canvas ref={canvasRef} />
+        {/*include loading message*/}
+        </section>
     )
 }
