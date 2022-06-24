d8c2f949d554af0ed4496f88483a047b1f9a7edc
GiuliaP95 <kirakira95@msn.com>
2022-06-24 14:12:16 +0200

    Little updates before testing p5 solution. To do: - link visualiser to music!! - https://blog.openreplay.com/authentication-in-react-18-using-firebase-v9 check this for ∆ between sign up, login, logout; - make login on-page; - customise palette for different themes.

--- src/pages/Visualiser.js
+++ src/pages/Visualiser.js
@@ -1,12 +1,21 @@
 import React, { useState} from "react";
 import {useEffect} from "react";
 import style from "../style/visualiser.module.scss"
-import p5 from "p5/lib/p5.min.js";
+
+// import p5 from "p5";
+import p5 from 'p5';
+// import 'p5/lib/addons/p5.sound';
+
+
+// import {p5sound} from "p5/lib/addons/p5.sound.min";
+//import 'p5/lib/addons/p5.sound';
 //note: 3 possible youtube links i.e. 3 possible ways that they key will be embedded
 //https://www.npmjs.com/package/youtube-mp3-downloader
 
 const a = 30;
-const b = .95;
+// const b = 0.95; //if shaky
+
+const spacing_x = 10;
 
 const glow_r = 255;
 const glow_g = 255;
@@ -21,18 +30,18 @@ const min_x = 0;
 
 const MAX_DEV = 100;
 
-
-
 export default function Visualiser(){
     const [songPlaying, setSongPlaying] = useState(false);
     const [song, setSong] = useState(undefined);
     const [audioCtx, setAudioCtx] = useState(undefined);
     const canvasRef = React.createRef()
     let p5ref;
+    const dataArray = new Uint8Array(32);
+    const [analyser, setAnalyser] = useState(undefined);
     // let song;
 
     useEffect(() => {
-        console.log('a load')
+        // console.log('a load')
         if (song !== undefined) {
             song.pause();
         }
@@ -47,6 +56,9 @@ export default function Visualiser(){
     }, [p5ref]);
 
     const Sketch = (p) => {
+        window.p5 = p;
+        let fft, amp;
+
         class myEffect {
             constructor(x, y, z) {
                 this.x = x;
@@ -80,7 +92,7 @@ export default function Visualiser(){
                 if (this.y > 200) {
 //       if not randomly, remove h parameter (or uncomment the if above)
                     this.y = h;
-                    this.x = h;
+                    this.x = spacing_x*h;
                     this.trail = [];
                 }
 
@@ -99,19 +111,28 @@ export default function Visualiser(){
         let test = new Array(30);
 
         p.setup = () => {
+            // amp = new p5.Amplitude();
+            // fft = new p5Class.FFT()
             console.log(test.length)
             for(let i=0;i<test.length;i++){
-                test[i] = new myEffect(50+i*50,10,1);
+                test[i] = new myEffect(50+i*50,10+i*50,1);
             }
             p.createCanvas(640, 480)
         }
 
         p.draw = () => {
             p.background(0);
+            // const vol = amp.getLevel();
+            // const freqs = fft.analyze()
+            // console.log(vol, freqs);
             const h = Math.random() * 200;
+            p.fill('red');
+            p.stroke('white');
             for(let i = 0;i<test.length;i++){
-                test[i].draw();
-                test[i].update(h);
+                //console.log(dataArray);
+                p.circle(50+50*i, dataArray[i+2], 20);
+                //test[i].draw();
+                //test[i].update(h);
             }
         }
 
@@ -125,23 +146,24 @@ export default function Visualiser(){
         setAudioCtx(audioContext);
         //console.log(audioContext.sampleRate);
         const audioSource = audioContext.createMediaElementSource(tune);
-        const analyser = audioContext.createAnalyser();
-        audioSource.connect(analyser);
-        analyser.connect(audioContext.destination);
-        analyser.fftSize = 64;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
+        const myAnalyser = audioContext.createAnalyser();
+        audioSource.connect(myAnalyser);
+        myAnalyser.connect(audioContext.destination);
+        myAnalyser.fftSize = 64;
+        setAnalyser(myAnalyser);
         //console.log(dataArray)
-
-        const tick = () => {
-            analyser.getByteTimeDomainData(dataArray);
-            //console.log(dataArray);
-            requestAnimationFrame(tick);
-        }
 
         return 'hi';
     }
 
+    // const tick = () => {
+    //     console.log(songPlaying,'songPlaying', analyser);
+    //     if (songPlaying && analyser !== undefined) {
+    //         analyser.getByteTimeDomainData(dataArray);
+    //         // console.log(dataArray);
+    //         requestAnimationFrame(tick);
+    //     }
+    // }
     //
     // const draw = (cRef) =>{
     //     cRef.width=500;
@@ -169,7 +191,6 @@ export default function Visualiser(){
         audioCtx.resume()
         setSongPlaying(true);
         song.play();
-
     }
 
     const pauseSong = () => {
